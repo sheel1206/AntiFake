180212ecb9b0e3a048d1d32160725fcc90de186a
sheel1206 <sheelpatel2001@gmail.com>
2021-11-29 17:32:15 +0530

    Change

--- app.py
+++ app.py
@@ -19,10 +19,10 @@ max_len = 50
 tokenized_pred = tokenized.texts_to_sequences(X)
 X = sequence.pad_sequences(tokenized_pred, maxlen=max_len)
 
-prediction = model.predict(X)
+prediction = model.predict(X) * 100
 print(prediction[0])
 if st.button("Detect"):
-        if prediction[0] <= 0.5:
+        if prediction[0] <= 50:
           st.success("Your news is FAKE with score of " + str((100 - prediction[0])))
         else:
             st.success("Your news is REAL with score of " + str(int(prediction[0])))
